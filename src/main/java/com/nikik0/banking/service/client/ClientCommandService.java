dd8a814054a6cd7590365285e612d107768fdc50
Nikita <nikitapolin47@gmail.com>
2024-02-27 14:08:06 +1000

    added client service interfaces

---
+++ src/main/java/com/nikik0/banking/service/client/ClientCommandService.java
@@ -0,0 +1,7 @@
+package com.nikik0.banking.service.client;
+
+import com.nikik0.banking.domain.model.Client;
+import com.nikik0.banking.service.CommandService;
+
+public interface ClientCommandService extends CommandService<Client> {
+}
