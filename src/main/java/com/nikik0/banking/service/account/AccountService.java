b93d0b353768bafc3f84897d493a8176155bcbe7
Nikik0 <nikitapolin47@gmail.com>
2024-03-15 10:44:48 +1000

    changed inheritance to composition

--- src/main/java/com/nikik0/banking/service/account/AccountService.java
+++ src/main/java/com/nikik0/banking/service/account/AccountService.java
@@ -1,7 +1,10 @@
 package com.nikik0.banking.service.account;
 
-import com.nikik0.banking.domain.model.Account;
 
-public interface AccountService extends AccountQueryService, AccountCommandService {
+import com.nikik0.banking.domain.model.Account;
+import com.nikik0.banking.service.CommandService;
+import com.nikik0.banking.service.QueryService;
+
+public interface AccountService extends QueryService<Account>, CommandService<Account> {
 
 }
--- src/main/java/com/nikik0/banking/service/transaction/TransactionService.java
+++ src/main/java/com/nikik0/banking/service/transaction/TransactionService.java
@@ -1,4 +1,8 @@
 package com.nikik0.banking.service.transaction;
 
-public interface TransactionService extends TransactionCommandService, TransactionQueryService{
+import com.nikik0.banking.domain.model.Transaction;
+import com.nikik0.banking.service.CommandService;
+import com.nikik0.banking.service.QueryService;
+
+public interface TransactionService extends CommandService<Transaction>, QueryService<Transaction> {
 }
